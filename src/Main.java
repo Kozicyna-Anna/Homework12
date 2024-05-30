0cfe8b365d892fe863f893eb18b36d94934a3f4c
New <Dar1985@bk.ru>
2024-05-31 00:39:29 +0700

    ДЗ переделано

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         Author kathy = new Author("Kathy", "Bert");
-        Book headFirstJava = new Book("Head First Java", kathy,2022);
+        Book headFirstJava = new Book("Head First Java", kathy, 2022);
 
+        System.out.println(headFirstJava.getTitle() + " " + headFirstJava.getPublisherYear());
+
+        headFirstJava.setPublisherYear(2020);
         System.out.println(headFirstJava.getTitle() + " " + headFirstJava.getPublisherYear());
     }
 }
